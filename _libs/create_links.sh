593310774508e4cc50ac27446cc65eaa0c570cda
Massimiliano Leone <maximilianus@gmail.com>
2015-06-09 17:16:45 +0200

    deleted some useless libs

--- _libs/create_links.sh
+++ _libs/create_links.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
 
 [ -z $ANDROID_HOME ] && echo ANDROID_HOME not setted && exit 1
+[ -z $OLYMPUS_PROJECTS ] && echo OLYMPUS_PROJECTS not setted && exit 1
 [ -z $ROBOGUICE_DIR ] && echo ROBOGUICE_DIR not setted && exit 1
 [ -z $ROBOGUICE_MAJOR_VERSION ] && echo "ROBOGUICE_MAJOR_VERSION (ex: 3.0) not setted" && exit 1
 [ -z $ROBOGUICE_COMPLETE_VERSION ] && echo "ROBOGUICE_COMPLETE_VERSION (ex: 3.0.1) not setted" && exit 1
 
-RM="echo rm -fv"
-LNS="echo ln -sv"
+RM="rm -fv"
+LNS="ln -fsv"
 
-[ ! -h android-support-v4.jar ] && $RM -f android-support-v4.jar && $LNS $ANDROID_HOME/extras/android/support/v4/android-support-v4.jar
+[ ! -h android-support-v4.jar ] && [ ! -e android-support-v4.jar ] && $RM android-support-v4.jar && $LNS $ANDROID_HOME/extras/android/support/v4/android-support-v4.jar
 
-for i in ../Diane/library/common/build/jars/diane-common.jar ../Diane/library/task/roboguiced/build/jars/diane-robotasks.jar ../Diane/library/utils/roboguiced/build/jars/diane-roboutils.jar ../Enhancedsafeasynctask/vanilla/library/build/jars/enhancedsafeasynctask.jar ../Epicurus/build/jar/epicurus.jar ../Socrates/library/build/jars/google-preconditions.jar ../Kusor/build/jars/kusor-common.jar ../Kusor/build/jars/kusor-roboguiced.jar ../Kusor/build/jars/kusor-tasks-roboguiced.jar ../MirroringException/jars/mirroringexception.jar ../Polaris/build/jars/polaris-core.jar ../Polaris/build/jars/polaris-core-roboguiced.jar ../Polaris/build/jars/polaris-observatory.jar ../Polaris/build/jars/polaris-utils.jar ../Enhancedsafeasynctask/roboguiced/library/build/jars/roboenhancedasynctask.jar ../Socrates/library/build/jars/socrates-base.jar ../Socrates/library/build/jars/socrates-events.jar ../Socrates/library/build/jars/socrates-roboguiced.jar ../Ulysses/library/common/build/jars/ulysses-common.jar ../Ulysses/library/common/build/jars/ulysses-roboguiced.jar ../Ulysses/library/ui/ui-jar/build/jar/ulysses-ui.jar; do
+for i in $OLYMPUS_PROJECTS/Diane/library/common/build/jars/diane-common.jar $OLYMPUS_PROJECTS/Diane/library/task/roboguiced/build/jars/diane-robotasks.jar $OLYMPUS_PROJECTS/Diane/library/utils/roboguiced/build/jars/diane-roboutils.jar $OLYMPUS_PROJECTS/Enhancedsafeasynctask/vanilla/library/build/jars/enhancedsafeasynctask.jar $OLYMPUS_PROJECTS/Epicurus/build/jar/epicurus.jar $OLYMPUS_PROJECTS/Socrates/library/build/jars/google-preconditions.jar $OLYMPUS_PROJECTS/Kusor/build/jars/kusor-common.jar $OLYMPUS_PROJECTS/Kusor/build/jars/kusor-roboguiced.jar $OLYMPUS_PROJECTS/MirroringException/jars/mirroringexception.jar $OLYMPUS_PROJECTS/Polaris/build/jars/polaris-core.jar $OLYMPUS_PROJECTS/Polaris/build/jars/polaris-core-roboguiced.jar $OLYMPUS_PROJECTS/Polaris/build/jars/polaris-task.jar $OLYMPUS_PROJECTS/Polaris/build/jars/polaris-task-roboguiced.jar $OLYMPUS_PROJECTS/Polaris/build/jars/polaris-observatory.jar $OLYMPUS_PROJECTS/Polaris/build/jars/polaris-utils.jar $OLYMPUS_PROJECTS/Enhancedsafeasynctask/roboguiced/library/build/jars/roboenhancedasynctask.jar $OLYMPUS_PROJECTS/Socrates/library/build/jars/socrates-base.jar $OLYMPUS_PROJECTS/Socrates/library/build/jars/socrates-events.jar $OLYMPUS_PROJECTS/Socrates/library/build/jars/socrates-roboguiced.jar $OLYMPUS_PROJECTS/Ulysses/library/common/build/jars/ulysses-common.jar $OLYMPUS_PROJECTS/Ulysses/library/common/build/jars/ulysses-roboguiced.jar $OLYMPUS_PROJECTS/Ulysses/library/ui/ui-jar/build/jar/ulysses-ui.jar; do
    file=$(echo $i | awk -F"/" '{print $NF}')
    # if real file, remove it
    [ -f $file ] && [ ! -h $file ] && $RM $file
@@ -20,7 +22,7 @@ done
 file="roboguice"
 # really, it's a dir
 [ -d $file ] && [ ! -h $file ] && $RM $file
-[ -e $file ] || $LNS $ROBOGUICE_DIR file
+[ -e $file ] || $LNS $ROBOGUICE_DIR $file
 
 file="roboguice.jar"
 [ -f $file ] && [ ! -h $file ] && $RM $file
--- etc/setenv.sh
+++ etc/setenv.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 declare -x OLYMPUS_HOME=$(pwd)
-
+( [ -z ${OLYMPUS_PROJECTS} ] || [ ! -d ${OLYMPUS_PROJECTS} ] ) && echo "OLYMPUS_PROJECTS environment variable is not setted with right directory (that is: dir containing Diane, Socrates, Ulysses, etc)"
 declare -x JAVA_VERSION="7"
 declare -x JAVA_VENDOR="oracle"
 declare -x J2REDIR="/usr/lib/jvm/java-$JAVA_VERSION-$JAVA_VENDOR/jre"
